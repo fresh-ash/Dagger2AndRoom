dc3916ccea734357ee0240f1e0e70e1893797d43
fresh-ash <sergei081@gmail.com>
2018-02-01 23:42:15 +0200

    1.0

--- app/src/main/java/com/example/ss/daggerandroidexample/App.java
+++ app/src/main/java/com/example/ss/daggerandroidexample/App.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.Application;
 
 import com.example.ss.daggerandroidexample.di.AppComponent;
+import com.example.ss.daggerandroidexample.di.DaggerAppComponent;
 
 
 import javax.inject.Inject;
@@ -22,8 +23,8 @@ public class App extends DaggerApplication implements HasActivityInjector {
 
     @Override
     protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
-        //AppComponent appComponent = DaggerAppComponent.builder().application(this).build();
-        //appComponent.inject(this);
-        return null;
+        AppComponent appComponent = DaggerAppComponent.builder().application(this).build();
+        appComponent.inject(this);
+        return appComponent;
     }
 }
